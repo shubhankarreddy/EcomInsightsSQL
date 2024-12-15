23c7ae6d1ff2281c8cf65d803fff020d6b25c663
Shubhankar Reddy <shubhankar.reddy94@gmail.com>
2024-12-16 01:44:51 +0530

    Add files via upload

--- sql_queries/top_selling_products.sql
+++ sql_queries/top_selling_products.sql
@@ -1,8 +1,8 @@
--- identify top selling products by year
 
 SELECT * FROM products; -- view table
 SELECT * FROM Sales; -- view table
 
+-- identify top selling products by year
 -- top selling products for year 2021
 
 CREATE VIEW sales_view_2021 AS
@@ -81,17 +81,58 @@ FROM ranked_sales
 WHERE s_rank <= 10
 ORDER BY s_rank;
 
+-- sales by region
+
+SELECT s.customer_region,
+       sum(s.total_sale_amount) as region_sales
+FROM sales s
+GROUP BY s.customer_region
+ORDER BY region_sales DESC;
+
 -- Total sales by category
 
-SELECT p.category, SUM(s.total_sale_amount) AS total_sales
+SELECT p.category,
+       SUM(s.total_sale_amount) AS total_sales
 FROM products p
 INNEr JOIN sales s on p.product_id = s.product_id
 GROUP BY p.category
 ORDER BY total_sales DESC;
 
--- sales by region
+-- top selling products
 
-SELECT s.customer_region, sum(s.total_sale_amount) as region_sales
-FROM sales s
-GROUP BY s.customer_region
-ORDER BY region_sales DESC;
+SELECT p.product_name,
+       sum(sa.total_Sale_amount) AS total_revenue,
+       sum(sa.quantity_sold) as total_qty_sold
+from products p
+INNER JOIN sales sa ON p.product_id = sa.product_id
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+
+-- calculating avg sales
+
+SELECT
+    AVG(total_sale_amount) AS avg_total_revenue,
+    AVG(quantity_sold) AS avg_quantity_sold
+FROM  sales;
+
+-- under performing products ( on the basis that product have high stock level but low sales)
+
+SELECT p.product_name,
+       p.category,
+       sum(sa.quantity_sold) AS qty_sold,
+       sum(sa.total_sale_amount) AS total_sales,
+       iy.current_stock,
+       iy.reorder_level
+FROM products p
+INNER JOIN sales sa ON p.product_id = sa.product_id
+INNER JOIN inventory iy ON p.product_id = iy.product_id
+GROUP BY
+    p.product_name, p.category, iy.current_stock, iy.reorder_level
+HAVING
+    (sum(sa.quantity_sold) < 4 OR sum(sa.total_sale_amount) < 6000) -- avg qty sold = 5 and avg_sale = 7040
+AND
+    iy.current_stock > iy.reorder_level
+ORDER BY
+    total_sales ASC , qty_sold ASC;
