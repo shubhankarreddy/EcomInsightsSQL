8a59f7a0420f8f5335bbff22802155e1c7b27e9c
Shubhankar Reddy <shubhankar.reddy94@gmail.com>
2024-12-16 14:11:07 +0530

    Delete sql_queries/2_top_selling_product.sql

--- sql_queries/2_top_selling_product.sql
+++
@@ -1,9 +0,0 @@
--- top selling product
-
-SELECT
-    p.product_name,
-    SUM(s.total_sale_amount) AS total_revenue
-FROM sales s
-JOIN products p ON s.product_id = p.product_id
-GROUP BY p.product_name
-ORDER BY total_revenue DESC;
