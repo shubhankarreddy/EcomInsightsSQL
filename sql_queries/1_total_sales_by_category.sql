8c6839cc45e845a3e824da66caa8737ca84ca0b0
Shubhankar Reddy <shubhankar.reddy94@gmail.com>
2024-12-16 14:03:55 +0530

    Delete sql_queries/1_total_sales_by_category.sql

--- sql_queries/1_total_sales_by_category.sql
+++
@@ -1,8 +0,0 @@
--- Total sales by category
-
-SELECT p.category,
-       SUM(s.total_sale_amount) AS total_sales
-FROM products p
-INNEr JOIN sales s on p.product_id = s.product_id
-GROUP BY p.category
-ORDER BY total_sales DESC;
